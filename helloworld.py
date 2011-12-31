1c668cf76bd17ccd7aa97f6e26c5cbe0b3f05fb8
Laba Joe <s15454@yahoo.com>
2011-12-31 17:44:31 +0800

    test on readme

--- helloworld.py
+++ helloworld.py
@@ -6,67 +6,6 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-
-
-class HWGreeting(db.Model):
-  author = db.UserProperty()
-  content = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
-
-
-class HWGuestbook(webapp.RequestHandler):
-  def post(self):
-    greeting = HWGreeting()
-
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
-
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/homework')
-
-
-class Homework(webapp.RequestHandler):
-     def get(self):
-	greetings_query = HWGreeting.all().order('-date')
-#	greetings_query.reverse()
-	greetings = greetings_query.fetch(10)
-	if users.get_current_user():
-	  url = users.create_logout_url(self.request.uri)
-	  url_linktext = 'Logout'
-	else:
-	  url = users.create_login_url(self.request.uri)
-	  url_linktext = 'To post on this guestbook, please login here'
-	
-	template_values = {
-	  'greetings': greetings,
-	  'url': url,
-	  'url_linktext': url_linktext,
-	  'login': users.get_current_user(),
-	  }
-	path = os.path.join(os.path.dirname(__file__), 'homework.html')
-	self.response.out.write(template.render(path, template_values))
-
-class Homeworkprofile(webapp.RequestHandler):
-     def get(self):
-	if users.get_current_user():
-	  url = users.create_logout_url(self.request.uri)
-	  url_linktext = 'Logout'
-	else:
-	  url = users.create_login_url(self.request.uri)
-	  url_linktext = 'Login'
-	
-	template_values = {
-	  'url': url,
-	  'url_linktext': url_linktext,
-	  'login': users.get_current_user(),
-	  }
-	path = os.path.join(os.path.dirname(__file__), 'homeworkprofile.html')
-	self.response.out.write(template.render(path, template_values))
-
-
-
-
 class Greeting(db.Model):
   author = db.UserProperty()
   content = db.StringProperty(multiline=True)
@@ -79,9 +18,6 @@ class Knightprofile(db.Model):
   time = db.StringProperty()
   fromm = db.StringProperty()
   to = db.StringProperty()
-  email = db.StringProperty()
-  day = db.StringProperty()
-  fb = db.StringProperty()
   submitdate = db.DateTimeProperty(auto_now_add=True)
 
 
@@ -92,22 +28,8 @@ class Riderprofile(db.Model):
   time = db.StringProperty()
   fromm = db.StringProperty()
   to = db.StringProperty()
-  email = db.StringProperty()
-  day = db.StringProperty()
-  fb = db.StringProperty()
   submitdate = db.DateTimeProperty(auto_now_add=True)
-  
-class Matchprofile(db.Model):
-	knight = db.UserProperty()
-	kname = db.StringProperty()
-	rider = db.UserProperty()
-	rname = db.StringProperty()
-	time = db.StringProperty()
-	fromm = db.StringProperty()
-	to = db.StringProperty()
-	submitdate = db.DateTimeProperty(auto_now_add=True)
-	
- 
+
 class MainPage(webapp.RequestHandler):
   def get(self):
 	greetings_query = Greeting.all().order('-date')
@@ -151,9 +73,6 @@ class Signknight(webapp.RequestHandler):
     kprofile.time = self.request.get('time')
     kprofile.fromm = self.request.get('from')
     kprofile.to = self.request.get('to')
-    kprofile.email= self.request.get('email')
-    kprofile.fb = self.request.get('fb')
-    kprofile.day = self.request.get('day')
     kprofile.put()
     self.redirect('/')
 
@@ -168,10 +87,6 @@ class Signrider(webapp.RequestHandler):
     rprofile.time = self.request.get('time')
     rprofile.fromm = self.request.get('from')
     rprofile.to = self.request.get('to')
-    rprofile.email= self.request.get('email')
-    rprofile.fb = self.request.get('fb')
-    rprofile.day = self.request.get('day')
-
     rprofile.put()
     self.redirect('/')
 
@@ -181,35 +96,11 @@ class Admin(webapp.RequestHandler):
 	kprofiles = kprofiles_query.fetch(10)
 	rprofiles_query = Riderprofile.all().order('-submitdate')
 	rprofiles = rprofiles_query.fetch(10)
-	
-	matchprofiles_query = Matchprofile.all().order('-submitdate')
-	for matchprofiles in matchprofiles_query :
-		matchprofiles.delete()
-		
-	for kprofile in kprofiles:
-		for rprofile in rprofiles:
-			if rprofile.time == kprofile.time:
-				Match = Matchprofile()
-				Match.knight = kprofile.user
-				Match.kname = kprofile.name
-				Match.rider = rprofile.user
-				Match.rname = rprofile.name
-				Match.time = rprofile.time
-				Match.fromm = rprofile.fromm
-				Match.to = rprofile.to				
-				Match.put()
-	
-	matchprofiles_query = Matchprofile.all().order('-submitdate')
-	matchprofiles = matchprofiles_query.fetch(10)
-	
 	template_values = {
-		'kprofiles': kprofiles,
-		'rprofiles': rprofiles,
-		'matchprofiles': matchprofiles,
-		'login': users.get_current_user(),
+      'kprofiles': kprofiles,
+      'rprofiles': rprofiles,
+      'login': users.get_current_user(),
       }
-
-
 	path = os.path.join(os.path.dirname(__file__), 'admin.html')
 	self.response.out.write(template.render(path, template_values))
 
@@ -249,7 +140,7 @@ class Knight(webapp.RequestHandler):
 
     path = os.path.join(os.path.dirname(__file__), 'knight.html')
     self.response.out.write(template.render(path, template_values))
-
+    
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
@@ -258,10 +149,7 @@ application = webapp.WSGIApplication(
                                       ('/rider', Rider),
                                       ('/signrider', Signrider),
                                       ('/signknight', Signknight),
-				      ('/admin', Admin),
-				      ('/homework', Homework),
-				      ('/homework/profile', Homeworkprofile),
-				      ('/homework/sign',HWGuestbook)
+				      ('/admin', Admin)
                                      ],
                                      debug=True)
 
